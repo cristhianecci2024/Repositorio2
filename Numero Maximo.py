0468b75302a26d7ff1e7d79e5235cca469392801
cristhianfelBar23 <cfbarragan@misena.edu.co>
2024-05-29 19:17:57 -0500

    agregue el archivo maximo

---
+++ Numero Maximo.py
@@ -0,0 +1,14 @@
+#tupla1=(5,3,1,"Hola")
+#tupla2=(3.1,"T",False,-124321,8)
+
+
+lista1=[5,1,4,8,-9,2]
+#lista2=[1,0,-1,7,-7]
+#lista3[4.01,5,"Hola",True]
+
+
+numero_mayor=-100000
+for valor in lista1:
+    if valor > numero_mayor:
+        numero_mayor=valor
+print("el numero mayor de la lista es",numero_mayor)
